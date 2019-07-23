3e224b494a8c6f546a0076a9cd57a79447295915
BasicPrimitives <info@basicprimitives.com>
2019-07-22 21:40:47 -0400

    Updates

---
+++ precache-manifest.473b415c9f84bc85db28c09c7b935d6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ba06ed773c63320ce48434bdd587a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f354a6b59c82ea2ec750",
+    "url": "/static/css/main.804824dd.chunk.css"
+  },
+  {
+    "revision": "146aae24e0dc5b5662b9",
+    "url": "/static/js/2.a52c00e0.chunk.js"
+  },
+  {
+    "revision": "f354a6b59c82ea2ec750",
+    "url": "/static/js/main.e3f49c04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
