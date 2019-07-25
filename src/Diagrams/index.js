ed0c2d222423c2aac83707de7cfaec362f26bdc4
BasicPrimitives <info@basicprimitives.com>
2019-07-25 08:29:11 -0400

    Added components property schemas to exports

--- src/Diagrams/index.js
+++ src/Diagrams/index.js
@@ -1,5 +1,29 @@
 import orgDiagram from './OrgDiagram';
 import famDiagram from './FamDiagram';
 
+import backgroundAnnotationConfig from './Schemas/BackgroundAnnotationConfig';
+import connectorAnnotationConfig from './Schemas/ConnectorAnnotationConfig';
+import famDiagramConfig from './Schemas/FamDiagramConfig';
+import famDiagramItemConfig from './Schemas/FamDiagramItemConfig';
+import highlightPathAnnotationConfig from './Schemas/HighlightPathAnnotationConfig';
+import labelAnnotationConfig from './Schemas/LabelAnnotationConfig';
+import orgDiagramConfig from './Schemas/OrgDiagramConfig';
+import orgDiagramItemConfig from './Schemas/OrgDiagramItemConfig';
+import paletteItemConfig from './Schemas/PaletteItemConfig';
+import shapeAnnotationConfig from './Schemas/ShapeAnnotationConfig';
+import templateConfig from './Schemas/TemplateConfig';
+
 export const OrgDiagram = orgDiagram;
 export const FamDiagram = famDiagram;
+
+export const BackgroundAnnotationConfig = backgroundAnnotationConfig;
+export const ConnectorAnnotationConfig = connectorAnnotationConfig;
+export const FamDiagramConfig = famDiagramConfig;
+export const FamDiagramItemConfig = famDiagramItemConfig;
+export const HighlightPathAnnotationConfig = highlightPathAnnotationConfig;
+export const LabelAnnotationConfig = labelAnnotationConfig;
+export const OrgDiagramConfig = orgDiagramConfig;
+export const OrgDiagramItemConfig = orgDiagramItemConfig;
+export const PaletteItemConfig = paletteItemConfig;
+export const ShapeAnnotationConfig = shapeAnnotationConfig;
+export const TemplateConfig = templateConfig;
